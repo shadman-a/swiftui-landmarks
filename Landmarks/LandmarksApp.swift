5d315f4a4ec2e66c4ba940b9322ea1dc6485ea89
shadman-a <ahmedshadman966@gmail.com>
2021-07-08 08:58:31 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shadman Ahmed on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
