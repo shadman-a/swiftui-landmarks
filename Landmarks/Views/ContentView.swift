4efb0761040ab05dcd4f9b7c82ae3eba93cb1a7f
shadman-a <ahmedshadman966@gmail.com>
2021-07-16 23:49:25 -0400

    completed favorites

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,43 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack() {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-                .padding(.top, -300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -120)
-            
-            VStack(alignment: .leading) {
-                Text("533 Bushwick Ave")
-                    .font(.title)
-                HStack {
-                    Text("Brooklyn, NY")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("16 Apartments")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-
-                Text("Some kind of text")
-                   
-        }
-            
-        }
-
+        LandmarkList()
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
